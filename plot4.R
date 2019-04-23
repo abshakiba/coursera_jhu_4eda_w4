d1f4b5b565cdca4c3026bd4b7852d64243ece081
abshakiba <abshakiba@users.noreply.github.com>
2019-04-23 01:06:06 -0500

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -16,6 +16,28 @@ trend <- as.data.frame(trend)
 names(trend) = c("Emissions")
   
 ggplot(trend,aes(row.names(trend), trend$Emissions))+
-  geom_bar(stat = "identity")
+  geom_bar(stat = "identity")library(plyr)
+
+nei <- readRDS("summarySCC_PM25.rds")
+scc <- readRDS("Source_Classification_Code.rds")
+
+png('plot5.png')
+
+veh_index <- grepl("vehicle", tolower(scc$SCC.Level.Two))
+scc_veh <- scc[veh_index,]$SCC
+
+sub_veh<- nei[ nei$SCC %in% scc_veh,]
+sub_veh<- sub_veh[sub_veh$fips == "24510",]
+
+trend <- with(sub_veh, tapply(Emissions, year, sum, na.rm=T))
+trend <- as.data.frame(trend)
+names(trend) = c("Emissions")
+  
+ggplot(trend,aes(row.names(trend), trend$Emissions))+
+  geom_bar(stat = "identity")+
+  labs(x="Years", y="Total PM 2.5")
+
+dev.off()
+
 
 dev.off()
